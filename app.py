6a6e4c40631ec723c9904579ad55d5a180328c22
Joseph Francis <josephfrancis100@gmail.com>
2019-08-31 22:28:05 -0500

    GET and POST

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def hello_world():
     some = "Hello WORLD!"
     return render_template("home.html", something=some)
+
+
+@app.route("/hello", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form["name"]
+        greet = request.form["greet"]
+        greeting = f"Hello {name}, {greet}"
+        return render_template("index.html", greeting=greeting)
+    else:
+        greeting = request.args.get("name", "Nobody")
+        return render_template("index.html", greeting=greeting)
 
 
 @app.route("/about")
